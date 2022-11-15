e8109a041e7bcb5a9b02bf2ccb041340045eab7f
Ana Marza <ana-cristina-marza@tutanota.com>
2022-11-15 12:55:39 +0000

    save/read correlation matrix

--- stats_viewer.py
+++ stats_viewer.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from scipy.stats.mstats import pearsonr
+from scipy.stats.stats import pearsonr
 from slice_viewer import MultiSliceViewer
 from tqdm import tqdm
 
@@ -9,7 +9,8 @@ from tqdm import tqdm
 class CorrelationViewer(MultiSliceViewer):
 
     def __init__(self, volume, title, colorbar=True, legend=False,
-                 cmap='rainbow'):
+                 cmap='rainbow', corr_mat_file='corr_mat.npy',
+                 pval_mat_file='pval_mat.npy'):
 
         # assumes volume is 3D
         try:
@@ -23,16 +24,32 @@ class CorrelationViewer(MultiSliceViewer):
         evolutions = np.reshape(self.surface_slice, [t, x*y]).T
 
         # self.corr_mat = np.corrcoef(evolutions)
-        corr_mat = np.empty([y*x, y*x])
-        pval_mat = np.empty([y*x, y*x])
+        try:
+            corr_mat = np.load(corr_mat_file)
+            pval_mat = np.load(pval_mat_file)
+        except FileNotFoundError:
 
-        evolutions = np.ma.masked_invalid(evolutions)
-        for i, evo1 in enumerate(tqdm(evolutions)):
-            for j, evo2 in enumerate(tqdm(evolutions, leave=False)):
-                corr_coef, pval = pearsonr(evo1, evo2)
-                corr_mat[i, j] = corr_coef
-                pval_mat[i, j] = corr_coef
+            corr_mat = np.empty([y*x, y*x])
+            pval_mat = np.empty([y*x, y*x])
 
+            for i, evo1 in enumerate(tqdm(evolutions)):
+                if np.isnan(evo1).any():
+                    continue
+                for j, evo2 in enumerate(tqdm(evolutions, leave=False)):
+                    if np.isnan(evo2).any():
+                        continue
+                    corr_coef, pval = pearsonr(evo1, evo2)
+                    corr_mat[i, j] = corr_coef
+                    pval_mat[i, j] = pval
+            np.save(corr_mat_file, corr_mat)
+            np.save(pval_mat_file, pval_mat)
+
+        # debug
+        print(f"min: {np.min(pval_mat)}")
+        print(f"max: {np.max(pval_mat)}")
+
+        self.corr_mat = corr_mat
+        self.pval_mat = pval_mat
         self.corr_loc = [x/2, y/2]
         self.time_steps, self.n_cols, self.n_rows = t, y, x
 
@@ -51,7 +68,7 @@ class CorrelationViewer(MultiSliceViewer):
         self.corr_ax_image = self.corr_ax.imshow(
             dummy_corr_map,
             origin='lower',
-            cmap='cividis'
+            cmap='rainbow'
         )
         self.fig.colorbar(self.corr_ax_image, ax=self.corr_ax)
 
