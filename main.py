f6b90429af3dc4891eaea48673f2550e2c7d7a60
Ana Marza <ana-cristina-marza@tutanota.com>
2022-12-05 16:52:52 +0000

    create main.py file which reads from commandline which method to use on data

---
+++ main.py
@@ -0,0 +1,27 @@
+import argparse
+from nccluster.workflows import KMeansWorkflow
+from nccluster.workflows import CorrelationWorkflow
+from nccluster.workflows import TSClusteringWorkflow
+
+# parse commandline arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--config", "-c", help="file to read configuration from,\
+                     if parameters not supplied interactively")
+parser.add_argument("--method", "-m", required=True,
+                    help=" which method to use to analyse\
+                    the data: km = k-means; ts = timeseries clustering;\
+                    corr = correlation viewer + hierarchical clustering"
+                    )
+args = parser.parse_args()
+
+if args.method == 'km':
+    km_workflow = KMeansWorkflow(args.config)
+    km_workflow.run()
+elif args.method == 'ts':
+    ts_workflow = TSClusteringWorkflow(args.config)
+    ts_workflow.run()
+elif args.method == 'corr':
+    corr_workflow = CorrelationWorkflow(args.config)
+    corr_workflow.run()
+else:
+    print("[!] Unrecognised method passed via commandline; see help (-h)")
