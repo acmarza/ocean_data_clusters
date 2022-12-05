69d9876271fbb1efb7785ee6022453117b14bcd1
Ana Marza <ana-cristina-marza@tutanota.com>
2022-12-05 16:17:56 +0000

    create correlation workflow

---
+++ correlation.py
@@ -0,0 +1,11 @@
+import argparse
+from nccluster.workflows import CorrelationWorkflow
+
+# define and parse commandline arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--config", "-c", help="file to read configuration from, \
+                    if parameters not supplied interactively")
+args = parser.parse_args()
+
+corr_workflow = CorrelationWorkflow(args.config)
+corr_workflow.run()
