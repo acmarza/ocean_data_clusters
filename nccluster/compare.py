ec9c0d619a3e87f5227147866dfcdd47d157d06f
Ana Marza <ana-cristina-marza@tutanota.com>
2022-12-10 17:06:21 +0200

    clean-up

--- nccluster/compare.py
+++ nccluster/compare.py
@@ -15,58 +15,61 @@ class ClusterMatcher:
 
         # the labels are 0 to n-1
         n_clusters = int(np.nanmax(self.labels_left)) + 1
-        shape = self.labels_left.shape
+        if n_clusters != int(np.nanmax(self.labels_right)) + 1:
+            print("[!] The maps have a different number of clusters")
+            exit()
 
         # initialise empty arrays
-        array_of_sets_1 = []
-        array_of_sets_2 = []
+        array_of_sets_left = []
+        array_of_sets_right = []
 
         # for every cluster (label), get all the indices that have
         # the current label and put them in a set
         for cluster in range(0, n_clusters):
             (indices_1, ) = np.where(labels_left_flat == cluster)
-            array_of_sets_1.append(set(indices_1))
+            array_of_sets_left.append(set(indices_1))
             (indices_2, ) = np.where(labels_right_flat == cluster)
-            array_of_sets_2.append(set(indices_2))
+            array_of_sets_right.append(set(indices_2))
 
         # reorder lists to have biggest sets first
-        array_of_sets_1.sort(key=lambda x: -len(x))
-        array_of_sets_2.sort(key=lambda x: -len(x))
+        array_of_sets_left.sort(key=lambda x: -len(x))
+        array_of_sets_right.sort(key=lambda x: -len(x))
 
         # quantify overlap between pairs of sets between the two label versions
-        overlap_matrix = np.zeros((n_clusters, n_clusters))
         pairings_scores = []
-        for i, left_set in enumerate(array_of_sets_1):
-            for j, right_set in enumerate(array_of_sets_2):
+        for i, left_set in enumerate(array_of_sets_left):
+            for j, right_set in enumerate(array_of_sets_right):
                 sym_diff_size = len(left_set.symmetric_difference(right_set))
                 intersection_size = len(left_set.intersection(right_set))
                 overlap = intersection_size/sym_diff_size
                 # print(f'{i} vs. {j}: {overlap:.2%}')
                 pairings_scores.append((i, j, overlap))
 
+        # start with the highest overlap scores
         pairings_scores.sort(reverse=True, key=lambda x: x[2])
-        print('sorted')
 
-        print(overlap_matrix)
-
+        # prepare an empty array to hold the 1:1 matching between
+        # labels on the left and right
         translation = np.full(n_clusters, np.nan)
 
+        # put the pairings with the highest scores in the translation array
+        # skipping when an entry already exists
         for pair in pairings_scores:
-
-            print(pair)
             left, right, score = pair
             if right not in translation and np.isnan(translation[left]):
                 translation[left] = right
-            print(translation)
 
-        for i, left_set in enumerate(array_of_sets_1):
+        # relabel the sets to match based on our translation function
+        for i, left_set in enumerate(array_of_sets_left):
             for idx in left_set:
                 labels_left_flat[idx] = translation[i]
 
-        for i, left_set in enumerate(array_of_sets_2):
-            for idx in left_set:
+        for i, right_set in enumerate(array_of_sets_right):
+            for idx in right_set:
                 labels_right_flat[idx] = i
 
+        # reshape translated 1D arrays and update attributes
+        shape = self.labels_left.shape
         self.labels_left = np.reshape(labels_left_flat, shape)
         self.labels_right = np.reshape(labels_right_flat, shape)
 
