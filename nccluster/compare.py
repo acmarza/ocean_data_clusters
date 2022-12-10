a7a63d2847d1e56fd7ee42c28a0d89c9a4f2c47e
Ana Marza <ana-cristina-marza@tutanota.com>
2022-12-10 16:53:41 +0200

    apparently working

--- nccluster/compare.py
+++ nccluster/compare.py
@@ -4,18 +4,18 @@ import matplotlib.pyplot as plt
 
 class ClusterMatcher:
 
-    def labels_from_arrays(self, labels_1, labels_2):
-        self.labels_1 = labels_1
-        self.labels_2 = labels_2
+    def labels_from_arrays(self, labels_left, labels_right):
+        self.labels_left = labels_left
+        self.labels_right = labels_right
 
     def match_labels(self):
         # flatten labels into 1D array
-        labels_1_flat = self.labels_1.flatten()
-        labels_2_flat = self.labels_2.flatten()
+        labels_left_flat = self.labels_left.flatten()
+        labels_right_flat = self.labels_right.flatten()
 
         # the labels are 0 to n-1
-        n_clusters = int(np.nanmax(self.labels_1)) + 1
-        shape = self.labels_1.shape
+        n_clusters = int(np.nanmax(self.labels_left)) + 1
+        shape = self.labels_left.shape
 
         # initialise empty arrays
         array_of_sets_1 = []
@@ -24,9 +24,9 @@ class ClusterMatcher:
         # for every cluster (label), get all the indices that have
         # the current label and put them in a set
         for cluster in range(0, n_clusters):
-            (indices_1, ) = np.where(labels_1_flat == cluster)
+            (indices_1, ) = np.where(labels_left_flat == cluster)
             array_of_sets_1.append(set(indices_1))
-            (indices_2, ) = np.where(labels_2_flat == cluster)
+            (indices_2, ) = np.where(labels_right_flat == cluster)
             array_of_sets_2.append(set(indices_2))
 
         # reorder lists to have biggest sets first
@@ -35,61 +35,57 @@ class ClusterMatcher:
 
         # quantify overlap between pairs of sets between the two label versions
         overlap_matrix = np.zeros((n_clusters, n_clusters))
-        for i, idx_set in enumerate(array_of_sets_1):
-            for j, cf_set in enumerate(array_of_sets_2):
-                union_size = len(idx_set.union(cf_set))
-                sym_diff_size = len(idx_set.symmetric_difference(cf_set))
-                mismatch = sym_diff_size / union_size
-                overlap = 1 - mismatch
-                print(f'{i} vs. {j}: {overlap:.2%}')
-                overlap_matrix[i, j] = int(100 * overlap)
+        pairings_scores = []
+        for i, left_set in enumerate(array_of_sets_1):
+            for j, right_set in enumerate(array_of_sets_2):
+                sym_diff_size = len(left_set.symmetric_difference(right_set))
+                intersection_size = len(left_set.intersection(right_set))
+                overlap = intersection_size/sym_diff_size
+                # print(f'{i} vs. {j}: {overlap:.2%}')
+                pairings_scores.append((i, j, overlap))
 
-        # translation = np.array([(0, 0) for i in range(0, n_clusters)])
-        pairings = []
+        pairings_scores.sort(reverse=True, key=lambda x: x[2])
+        print('sorted')
 
-        for cluster in range(0, n_clusters**2):
-
-            # print(overlap_matrix)
-            # print("")
-            # score = overlap_matrix.max()
-            row, col = np.unravel_index(overlap_matrix.argmax(),
-                                        overlap_matrix.shape)
-            score = overlap_matrix.max()
-            overlap_matrix[row, col] = 0
-            pairings.append((row, col, score))
+        print(overlap_matrix)
 
         translation = np.full(n_clusters, np.nan)
-        for pairing in pairings:
-            print(pairing)
-            row, col, score = pairing
-            if col not in translation:
-                translation[row] = col
 
-        for i, idx_set in enumerate(array_of_sets_1):
-            for idx in idx_set:
-                labels_1_flat[idx] = translation[i]
+        for pair in pairings_scores:
 
-        for i, idx_set in enumerate(array_of_sets_2):
-            for idx in idx_set:
-                labels_2_flat[idx] = i
+            print(pair)
+            left, right, score = pair
+            if right not in translation and np.isnan(translation[left]):
+                translation[left] = right
+            print(translation)
 
-        self.labels_1 = np.reshape(labels_1_flat, shape)
-        self.labels_2 = np.reshape(labels_2_flat, shape)
+        for i, left_set in enumerate(array_of_sets_1):
+            for idx in left_set:
+                labels_left_flat[idx] = translation[i]
+
+        for i, left_set in enumerate(array_of_sets_2):
+            for idx in left_set:
+                labels_right_flat[idx] = i
+
+        self.labels_left = np.reshape(labels_left_flat, shape)
+        self.labels_right = np.reshape(labels_right_flat, shape)
 
     def load_labels(self, save_path_1, save_path_2):
         with np.load(save_path_1) as npz:
-            self.labels_1 = np.ma.MaskedArray(**npz)
+            self.labels_left = np.ma.MaskedArray(**npz)
         with np.load(save_path_2) as npz:
-            self.labels_2 = np.ma.MaskedArray(**npz)
+            self.labels_right = np.ma.MaskedArray(**npz)
 
     def save_labels(self, save_path_1, save_path_2):
         np.savez_compressed(save_path_1,
-                            data=self.labels_1.data, mask=self.labels_1.mask)
+                            data=self.labels_left.data,
+                            mask=self.labels_left.mask)
         np.savez_compressed(save_path_2,
-                            data=self.labels_2.data, mask=self.labels_2.mask)
+                            data=self.labels_right.data,
+                            mask=self.labels_right.mask)
 
     def compare_maps(self):
         fig, (ax1, ax2) = plt.subplots(1, 2)
-        ax1.imshow(self.labels_1, origin='lower')
-        ax2.imshow(self.labels_2, origin='lower')
+        ax1.imshow(self.labels_left, origin='lower')
+        ax2.imshow(self.labels_right, origin='lower')
         plt.show()
