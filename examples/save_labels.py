a15ee6a05180635925dbe884dc6aaf2595ebe856
Ana Marza <ana-cristina-marza@tutanota.com>
2022-12-22 23:03:40 +0200

    add commandline option to choose between ts and twostep methods

--- examples/save_labels.py
+++ examples/save_labels.py
@@ -4,20 +4,28 @@ import sys
 sys.path.append('.')
 
 from nccluster.workflows import TimeSeriesClusteringWorkflow
+from nccluster.workflows import TwoStepTimeSeriesClusterer
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--config',
                         help="config file for time series clustering")
-    parser.add_argument('-l', '--long_name',
-                        help="long name for nc variable")
+    parser.add_argument('-m', '--method',
+                        help='which method to use (ts = time series clustering\
+                        ;two = two-step time series clustering)')
     parser.add_argument('-f', '--file',
                         help="save file (.nc) for resulting labels")
     args = parser.parse_args()
-    wf = TimeSeriesClusteringWorkflow(args.config)
-    wf.fit_model()
-    wf.save_clustering_results(args.file, args.long_name)
+    if args.method == 'ts':
+        wf = TimeSeriesClusteringWorkflow(args.config)
+    elif args.method == 'two':
+        wf = TwoStepTimeSeriesClusterer(args.config)
+    else:
+        print(f"[!] Unrecognised option {args.method}")
+
+    wf.cluster()
+    wf.save_clustering_results(args.file)
 
 
 if __name__ == "__main__":
