a9c605b065b42a6110c12dd16e8ff8d12b375ec2
Ana Marza <ana-cristina-marza@tutanota.com>
2023-01-11 15:22:57 +0200

    change arguments

--- examples/DdR.py
+++ examples/DdR.py
@@ -7,15 +7,15 @@ from nccluster.compare import DdR_Histogram
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-l', '--left',
-                        help='the config file for the first dataset')
-    parser.add_argument('-r', '--right',
-                        help='the config file for the second dataset')
-    parser.add_argument('-c', '--clusters',
+    parser.add_argument('-c', '--config',
+                        help='the config file for the dataset')
+    parser.add_argument('-l', '--labels',
                         help='.nc file containing subcluster labels')
+    parser.add_argument('-m', '--medoids',
+                        help='the pickle file containing cluster centroids')
     args = parser.parse_args()
 
-    DdR_Histogram(args.left, args.right, args.clusters)
+    DdR_Histogram(args.config, args.labels, args.medoids)
 
 
 if __name__ == "__main__":
